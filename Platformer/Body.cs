c50df931db56d5ff4ea8b2a88bb175c2456512cb
teagu <tscortez97@gmail.com>
2021-10-17 15:26:19 -0300

    Removed Namespaces

--- Platformer/Body.cs
+++ Platformer/Body.cs
@@ -1,48 +1,45 @@
 using UnityEngine;
 
-namespace GAME001.Behaviours
+public class Body : MonoBehaviour
 {
-    public class Body : MonoBehaviour
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private Vector2 size;
+
+    public Vector2 Center => (Vector2)transform.position + offset;
+    public Vector2 Size => size;
+
+    public void Move(Vector2 motion)
     {
-        [SerializeField] private Vector2 offset;
-        [SerializeField] private Vector2 size;
+        CheckCollision(ref motion, 0);
+        CheckCollision(ref motion, 1);
+        transform.position += (Vector3)motion;
+    }
 
-        public Vector2 Center => (Vector2)transform.position + offset;
-        public Vector2 Size => size;
+    public bool IsOverlaping(Vector2 offset = default)
+    {
+        var point = Center + offset;
+        var size = Size - Vector2.one;
+        return Physics2D.OverlapBox(point, size, 0);
+    }
 
-        public void Move(Vector2 motion)
+    protected void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(Center, Size);
+    }
+
+    private void CheckCollision(ref Vector2 motion, int index)
+    {
+        var axis = new Vector2[] { Vector2.right, Vector2.up };
+        var origin = Center + axis[index] * 0.5F * Mathf.Sign(motion[index]);
+        var size = Size - Vector2.one;
+        var direction = motion * axis[index];
+        var distance = Mathf.Abs(motion[index]);
+        var hit = Physics2D.BoxCast(origin, size, 0, direction, distance);
+
+        if (hit)
         {
-            CheckCollision(ref motion, 0);
-            CheckCollision(ref motion, 1);
-            transform.position += (Vector3)motion;
-        }
-
-        public bool IsOverlaping(Vector2 offset = default)
-        {
-            var point = Center + offset;
-            var size = Size - Vector2.one;
-            return Physics2D.OverlapBox(point, size, 0);
-        }
-
-        protected void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(Center, Size);
-        }
-
-        private void CheckCollision(ref Vector2 motion, int index)
-        {
-            var axis = new Vector2[] { Vector2.right, Vector2.up };
-            var origin = Center + axis[index] * 0.5F * Mathf.Sign(motion[index]);
-            var size = Size - Vector2.one;
-            var direction = motion * axis[index];
-            var distance = Mathf.Abs(motion[index]);
-            var hit = Physics2D.BoxCast(origin, size, 0, direction, distance);
-
-            if (hit)
-            {
-                motion[index] = hit.distance * Mathf.Sign(motion[index]);
-            }
+            motion[index] = hit.distance * Mathf.Sign(motion[index]);
         }
     }
 }
--- Platformer/PlayerMover.cs
+++ Platformer/PlayerMover.cs
@@ -1,88 +1,84 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using GAME001.Behaviours;
 
-namespace GAME001.Player
+[RequireComponent(typeof(Body))]
+public class PlayerMover : MonoBehaviour
 {
-    [RequireComponent(typeof(Body))]
-    public class PlayerMover : MonoBehaviour
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpStrength;
+
+    private Body body;
+    private Vector2 motion;
+    private float coyoteTime;
+    private float jumpBuffer = -1;
+
+    protected void Awake()
     {
-        [SerializeField] private float moveSpeed;
-        [SerializeField] private float gravity;
-        [SerializeField] private float jumpStrength;
+        body = GetComponent<Body>();
+        InitializeInputs();
+    }
 
-        private Body body;
-        private Vector2 motion;
-        private float coyoteTime;
-        private float jumpBuffer = -1;
+    private void InitializeInputs()
+    {
+        var input = new GameInput();
+        input.Enable();
+        input.Gameplay.HorizontalMovement.performed += OnHorizontalMovement;
+        input.Gameplay.HorizontalMovement.canceled += OnHorizontalMovement;
+        input.Gameplay.Jump.performed += OnJump;
+        input.Gameplay.Jump.canceled += OnCancelJump;
+    }
 
-        protected void Awake()
+    protected void Update()
+    {
+        UpdateGravity();
+        CheckForJump();
+        body.Move(motion * Time.deltaTime);
+    }
+
+    private void OnHorizontalMovement(InputAction.CallbackContext context)
+    {
+        motion.x = context.ReadValue<float>() * moveSpeed;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        jumpBuffer = Time.time;
+    }
+
+    private void OnCancelJump(InputAction.CallbackContext context)
+    {
+        if (motion.y <= 0) return;
+        motion.y *= 0.6F;
+    }
+
+    private void UpdateGravity()
+    {
+        var isGrounded = body.IsOverlaping(Vector2.down);
+        var onCeiling = body.IsOverlaping(Vector2.up);
+
+        if (onCeiling && motion.y > 0)
         {
-            body = GetComponent<Body>();
-            InitializeInputs();
+            motion.y = 0;
         }
-
-        private void InitializeInputs()
+        else if (!isGrounded)
         {
-            var input = new GameInput();
-            input.Enable();
-            input.Gameplay.HorizontalMovement.performed += OnHorizontalMovement;
-            input.Gameplay.HorizontalMovement.canceled += OnHorizontalMovement;
-            input.Gameplay.Jump.performed += OnJump;
-            input.Gameplay.Jump.canceled += OnCancelJump;
+            motion.y -= gravity * Time.deltaTime;
         }
-
-        protected void Update()
+        else if (motion.y < 0)
         {
-            UpdateGravity();
-            CheckForJump();
-            body.Move(motion * Time.deltaTime);
+            motion.y = 0;
         }
+    }
 
-        private void OnHorizontalMovement(InputAction.CallbackContext context)
-        {
-            motion.x = context.ReadValue<float>() * moveSpeed;
-        }
+    private void CheckForJump()
+    {
+        coyoteTime = body.IsOverlaping(Vector2.down) ? Time.time : coyoteTime;
 
-        private void OnJump(InputAction.CallbackContext context)
-        {
-            jumpBuffer = Time.time;
-        }
+        if (jumpBuffer + 0.1F < Time.time) return;
+        if (coyoteTime + 0.1F < Time.time) return;
 
-        private void OnCancelJump(InputAction.CallbackContext context)
-        {
-            if (motion.y <= 0) return;
-            motion.y *= 0.6F;
-        }
-
-        private void UpdateGravity()
-        {
-            var isGrounded = body.IsOverlaping(Vector2.down);
-            var onCeiling = body.IsOverlaping(Vector2.up);
-
-            if (onCeiling && motion.y > 0)
-            {
-                motion.y = 0;
-            }
-            else if (!isGrounded)
-            {
-                motion.y -= gravity * Time.deltaTime;
-            }
-            else if (motion.y < 0)
-            {
-                motion.y = 0;
-            }
-        }
-
-        private void CheckForJump()
-        {
-            coyoteTime = body.IsOverlaping(Vector2.down) ? Time.time : coyoteTime;
-
-            if (jumpBuffer + 0.1F < Time.time) return;
-            if (coyoteTime + 0.1F < Time.time) return;
-
-            motion.y = jumpStrength;
-            jumpBuffer = 0;
-        }
+        motion.y = jumpStrength;
+        jumpBuffer = 0;
     }
 }
